775056271ce5bf4eb44aa092dfa18545aaac6123
Hugo Priori <priori.hugo@estudante.ufjf.br>
2021-11-29 22:56:11 -0300

    tirando o resize e adicionando as sprites do ken

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const canvas = document.body.getElementById("gameScreen");
+const canvas = document.getElementById('gameScreen');
 const ctx = canvas.getContext("2d");
+vw = (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9) 
+vh = (Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) ) 
+canvas.width = vw;
+canvas.height = vh; 
 
-window.addEventListener('resize' , ()=>{
-    let vw = (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9) // 90% da tela
-    let vh = (Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) *0.9) // 90% da tela
-    canvas.width = vw;
-    canvas.height = vh; 
-})
+const ken = new Image();
+ken.src = "assets\\ken.png";
+
+const larguraImagem = 70;
+const alturaImagem = 80;
