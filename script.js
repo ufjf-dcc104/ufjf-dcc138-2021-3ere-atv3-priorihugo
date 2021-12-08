7282eecf36c7989b03f203b04b0e9c75a2e57ad0
Hugo Priori <priori.hugo@estudante.ufjf.br>
2021-12-08 20:50:54 -0300

    modificando funcao desenha em funcao do objeto

--- script.js
+++ script.js
@@ -11,19 +11,19 @@ canvas.height = 400;
 const haduken = new Image();
 haduken.src = "assets\\haduken_strip10.png";
 const idle = new Image();
-idle.scr = "assets\\idle_strip9.png";
+idle.src = "assets\\idle_strip9.png";
 const idle2 = new Image();
-idle2.scr = "assets\\idle2_strip6.png";
+idle2.src = "assets\\idle2_strip6.png";
 const kick = new Image();
-kick.scr = "assets\\kick_strip3.png";
+kick.src = "assets\\kick_strip3.png";
 const kick2 = new Image();
-kick2.scr = "assets\\kick2_strip6.png";
+kick2.src = "assets\\kick2_strip6.png";
 const punch = new Image();
-punch.scr = "assets\\punch1_strip2.png";
+punch.src = "assets\\punch1_strip2.png";
 const punch2 = new Image();
-punch2.scr = "assets\\punch2_strip6.png";
+punch2.src = "assets\\punch2_strip6.png";
 const uppercut = new Image();
-uppercut.scr = "assets\\uppercut_strip6.png";
+uppercut.src = "assets\\uppercut_strip6.png";
 
 
 let acao = 0 , quadro = 0;
@@ -32,30 +32,30 @@ let personagem = {
     x: 80,
     y: 80,
     POSES: [
-        { img: idle, largura: 75, nFrames: 9, vFrame:7 },
-        { img: idle2, largura: 73, nFrames: 6, vFrame:7 },
-        { img: kick, largura: 123, nFrames: 3, vFrame:4 },
-        { img: kick2, largura: 114, nFrames: 6, vFrame:7 },
-        { img: punch, largura: 112, nFrames: 2, vFrame:4 },
-        { img: punch2, largura: 114, nFrames: 6, vFrame:7 },
-        { img: uppercut, largura: 83, nFrames: 6, vFrame:7 },
-        { img: haduken,  largura: 149, nFrames: 10, vFrame:7 }  
+        { img: idle, largura: 75, nFrames: 9, vFrame:9 },
+        { img: idle2, largura: 73, nFrames: 6, vFrame:6 },
+        { img: kick, largura: 123, nFrames: 3, vFrame:3 },
+        { img: kick2, largura: 114, nFrames: 6, vFrame:6 },
+        { img: punch, largura: 112, nFrames: 2, vFrame:2 },
+        { img: punch2, largura: 114, nFrames: 6, vFrame:6 },
+        { img: uppercut, largura: 83, nFrames: 6, vFrame:6 },
+        { img: haduken,  largura: 149, nFrames: 10, vFrame:10 }  
     ],
     desenha(){
 
         ctx.clearRect(0 , 0 , canvas.width , canvas.height);
         ctx.drawImage(
-            ken,///source
+            personagem.POSES[acao].img,///source
             ///posicao na sprite sheet
-            larguraImagem*Math.floor(quadro), //sx
-            alturaImagem*personagem.POSES[acao].posicao,
-            larguraImagem,
-            alturaImagem,
+            personagem.POSES[acao].largura*Math.floor(quadro), //sx
+            0,
+            personagem.POSES[acao].largura,
+            personagem.POSES[acao].img.height,
             ///posicao na tela;
             personagem.x,
             personagem.y,
-            larguraImagem*2,
-            alturaImagem*2   
+            personagem.POSES[acao].largura,
+            personagem.POSES[acao].img.height   
         );
 
     }
