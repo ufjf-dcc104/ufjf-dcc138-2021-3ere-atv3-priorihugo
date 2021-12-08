077c608d896f3c7a7c323815c48d82f19c14cf31
Hugo Priori <priori.hugo@estudante.ufjf.br>
2021-12-08 20:04:17 -0300

    alterando sprites

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const canvas = document.getElementById('gameScreen');
 const ctx = canvas.getContext("2d");
-vw = (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9) 
-vh = (Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) ) 
-canvas.width = vw;
-canvas.height = vh; 
+//vw = (Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) * 0.9) 
+//vh = (Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) ) 
+//canvas.width = vw;
+//canvas.height = vh; 
+canvas.width = 400;
+canvas.height = 400;
 
-const ken = new Image();
-ken.src = "assets\\ken.png";
+
+const haduken = new Image();
+haduken.src = "assets\\haduken_strip10.png";
+const idle = new Image();
+idle.scr = "assets\\idle_strip9.png";
+const idle2 = new Image();
+idle2.scr = "assets\\idle2_strip6.png";
+const kick = new Image();
+kick.scr = "assets\\kick_strip3.png";
+const kick2 = new Image();
+kick2.scr = "assets\\kick2_strip6.png";
+const punch = new Image();
+punch.scr = "assets\\punch1_strip2.png";
+const punch2 = new Image();
+punch2.scr = "assets\\punch2_strip6.png";
+const uppercut = new Image();
+uppercut.scr = "assets\\uppercut_strip6.png";
 
 const larguraImagem = 70;
 const alturaImagem = 80;
@@ -19,9 +36,9 @@ let personagem = {
     POSES: [
         { posicao: 0, nFrames: 4, vFrame:7 },
         { posicao: 1, nFrames: 4, vFrame:7 },
-        { posicao: 2, nFrames: 3, vFrame:7 },
+        { posicao: 2, nFrames: 3, vFrame:4 },
         { posicao: 3, nFrames: 4, vFrame:7 },
-        { posicao: 6, nFrames: 5, vFrame:7 },
+        { posicao: 6, nFrames: 5, vFrame:4 },
         { posicao: 7, nFrames: 5, vFrame:7 },
         { posicao: 8, nFrames: 7, vFrame:7 },  
     ],
@@ -38,8 +55,8 @@ let personagem = {
             ///posicao na tela;
             personagem.x,
             personagem.y,
-            larguraImagem,
-            alturaImagem   
+            larguraImagem*2,
+            alturaImagem*2   
         );
 
     }
@@ -53,7 +70,8 @@ function loop(t){
     dt = (t - t0)/1000;
 
     personagem.desenha();
-    quadro = (quadro > personagem.POSES[acao].nFrames)? quadro = 0 : quadro + personagem.POSES[acao].vFrame*dt;
+    quadro = (quadro >= personagem.POSES[acao].nFrames)? quadro = 0 : quadro + personagem.POSES[acao].vFrame*dt;
+    
     
     requestAnimationFrame(loop);
     t0 = t;
